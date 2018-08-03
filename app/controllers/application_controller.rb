17329fda51ae8cedc1e0ee8ec4f86e2cf0e940d4
21Miya <mmshin@live.com>
2018-08-03 15:37:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,43 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-  	erb :index
+  	erb :homebase
   end
+  
+  get "/selfcarequiz" do
+    erb :selfcarequiz
+  end 
+  
+  post '/selfcarequizresults' do
+    "You need #{params[:age]} hours of sleep each night and around #{params[water]} of water per day."
+  end 
+  
+  get '/peaceofmind'do
+    erb :peaceofmind
+  end
+  
+  get '/physicalhealth'do
+    erb :physicalhealth
+  end
+  
+  get '/areyouvalid'do
+    erb :areyouvalid
+  end
+  
+  get '/contactus'do
+    erb :contactus
+  end
+  
+  get '/homebase'do
+    erb :homebase
+  end
+  
+  get '/metime'do
+    erb :metime
+  end
+  
+   post '/areyouvalidresults' do
+	"#{params[:name]}, you're #{params[:gender]}, #{params[:race]}, and #{params[:sexuality]}. But you're also incredibly #{params[:trait]}--and 100% valid!"
+  end
+  
 end
